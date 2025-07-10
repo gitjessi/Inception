4d2c3824c6461ebc079cdabc33a5a0c34433acee
jessica <jessica.l59@outlook.fr>
2025-07-10 17:01:33 +0200

    mariadb

---
+++ srcs/requirements/mariadb/init.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+if [ ! -d /var/lib/mysql/mysql ]; then
+  echo "Initialisation de la base de donnees..."
+  mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql > /dev/null
+
+  mysqld --user=mysql --bootstrap <<EOF
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
+fi
+
+exec mysqld --user=mysql --console #--console message d erreur sur la console
+
+
+
+
